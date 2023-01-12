d34b56e51fe1251fff2926769ba44d09b01ac481
Family use <pks@PonkarteyansMBP.fuhsd.org>
2023-01-11 19:32:50 -0800

    more stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <img src={require("./Field_Map_2023.png")} height={"140%"} width={"138%"}/>
+      <img src={require("./Field_Map_2023.png")} height={"138%"} width={"138%"}/>
     </div>
   );
 }
