7c5c0ed7cc9e3b1cfb6ea48c07b4e2aacdb782c3
kgabc <kushgoel08@gmail.com>
2023-01-11 22:57:58 -0800

    Fixed button issue

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   const [state, setState] = React.useState([])
 
   function handleClick(event) {
+    if(event.target==event.currentTarget){
     const xpos = event.clientX
     const ypos = event.clientY
-    const fieldHeight = (315.5/739.68)*(739.68-ypos)
-    const fieldWidth = (651.25/1523.52)*xpos
+    const fieldHeight = ((315.5/739.68)*(739.68-ypos))
+    const fieldWidth = ((651.25/1523.52)*xpos)
     setState(prevState => [...prevState, {"x": xpos, "y": ypos, "fieldx": fieldWidth, "fieldy": fieldHeight}])
     console.log([fieldHeight, fieldWidth])
+    }
   }
 
   function handleRemove(xpos, ypos) {
@@ -30,14 +32,14 @@ function App() {
   }
 
   return (
-    <div className="container" onClick={handleClick}>
-      <img style={{"position": "relative"}} src={require("./Field_Map_2023.png")} height={pictureDims.height} width={pictureDims.width}/>
+    <div className="container">
+      <img style={{"position": "relative"}} onClick={handleClick} src={require("./Field_Map_2023.png")} height={pictureDims.height} width={pictureDims.width}/>
       {state.map((value, index) => {
         return (
-          <div style={{"position": "absolute", "left": value.x, "top": value.y, "backgroundColor": "yellow"}}>
-            {value.fieldx}, {value.fieldy}
+          <div style={{"position": "absolute", "left": value.x, "top": value.y, "backgroundColor": "#e87272", "padding":"2px"}}>
+            {Math.floor(value.fieldx/12)}' {(value.fieldx%12).toFixed(2)}", {Math.floor(value.fieldy/12)}' {(value.fieldy%12).toFixed(2)}"
             <button onClick={() => handleRemove(value.x, value.y)}>
-              remove
+              Remove
             </button>
           </div>
         )
