13cbb65063d1f7eee9802163da23bda37cd467ef
kgabc <kushgoel08@gmail.com>
2023-01-12 22:06:12 -0800

    Added Marker Func.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     if (event.target == event.currentTarget) {
       const xpos = event.clientX;
       const ypos = event.clientY;
+      console.log(xpos, ypos)
       const fieldHeight = (315.5 / 739.68) * (739.68 - ypos);
       const fieldWidth = (651.25 / 1523.52) * xpos;
       setState((prevState) => [
@@ -18,14 +19,25 @@ function App() {
       console.log([fieldHeight, fieldWidth]);
     }
   }
+  function handleClickMark(text) {
+    var element = document.getElementById(text);
+    console.log(element.style.display)
+    if(element.style.display=="none"){
+      element.style.display="block";
+    }
+    else{
+      element.style.display="none"
+    }
+  }
 
-  function handleRemove(xpos, ypos) {
-    console.log("remove coords", [xpos, ypos]);
-    var cur = state;
-    cur = cur.filter((vals) => {
-      return vals.x != xpos || vals.y != ypos;
-    });
-    setState(cur);
+  function handleMarkRemove(text1, text2){
+    console.log("remove coords", [text1]);
+    var element = document.getElementById(text1);
+    element.remove();
+    var element2 = document.getElementById(text2);
+    if(element2!=undefined){
+      element2.remove();
+    }
   }
 
   const pictureDims = {
@@ -45,6 +57,7 @@ function App() {
       {state.map((value, index) => {
         return (
           <div
+            id={`${Math.floor(value.fieldx / 12)}' ${(value.fieldx % 12).toFixed(2)}", ${Math.floor(value.fieldy / 12)}' ${(value.fieldy % 12).toFixed(2)}"`}
             style={{
               position: "absolute",
               left: value.x,
@@ -57,9 +70,27 @@ function App() {
           >
             {Math.floor(value.fieldx / 12)}' {(value.fieldx % 12).toFixed(2)}",{" "}
             {Math.floor(value.fieldy / 12)}' {(value.fieldy % 12).toFixed(2)}"
-            <div className="btn" onClick={() => handleRemove(value.x, value.y)}>
-              Remove
-            </div>
+          </div>
+        );
+      })}
+      {state.map((value, index) => {
+        return (
+          <div
+            id={`${Math.floor(value.fieldx / 12)}' ${(value.fieldx % 12).toFixed(2)}", ${Math.floor(value.fieldy / 12)}' ${(value.fieldy % 12).toFixed(2)}" dot`}
+            style={{
+              position: "absolute",
+              left: value.x-7,
+              top: value.y-7,
+              height: "18px", 
+              width: "18px",
+              backgroundColor: "#f5e218",
+              display: "flex-row",
+              borderRadius: "9px",
+            }}
+            onClick={() => handleClickMark(`${Math.floor(value.fieldx / 12)}' ${(value.fieldx % 12).toFixed(2)}", ${Math.floor(value.fieldy / 12)}' ${(value.fieldy % 12).toFixed(2)}"`)}
+            onContextMenu={() => handleMarkRemove(`${Math.floor(value.fieldx / 12)}' ${(value.fieldx % 12).toFixed(2)}", ${Math.floor(value.fieldy / 12)}' ${(value.fieldy % 12).toFixed(2)}" dot`, `${Math.floor(value.fieldx / 12)}' ${(value.fieldx % 12).toFixed(2)}", ${Math.floor(value.fieldy / 12)}' ${(value.fieldy % 12).toFixed(2)}"`)}
+          >
+            <div style={{position: "absolute", left:"5px", top: "5px", height:"8px", width:"8px", borderRadius:"4px", backgroundColor: "#FFFFFF"}}></div>
           </div>
         );
       })}
